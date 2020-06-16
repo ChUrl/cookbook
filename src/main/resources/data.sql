6e2372fdc28606bfebba232f4b140d66f914dff0
Christoph <churl100@hhu.de>
2020-06-16 21:28:58 +0200

    data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,67 @@
+INSERT INTO recipe(title, description, body)
+VALUES ('Gefülltes Fladenbrot', 'Gefülltes Fladenbrot mit Schafskäse, Gurken und Krautsalat',
+        '= Document Title
+Pingu <pingu@pongu.com>
+
+== The documents provides:
+=== The documents provides:
+
+Das ist ein normaler Paragraph. Mit mehreren Sätzen.
+Und mehreren Zeilen. SO wie es sein sollte, bla bla bla Lorem Ipsum Dolockuhokuspokus
+
+* List
+** List
+*** List
+
+Die Liste sollte hier zuende sein.
+
+. List
+. List
+.. List
+.. List
+.. List
+
+Warum muss man jede Liste mit Text beenden? Aber egal einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text,
+einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text,
+einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text Hurraaaaaaaa!!!!
+
+Ein AQbsatz
+
+Warum muss man jede Liste mit Text beenden? Aber egal einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text,
+einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text,
+einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text einfach mehr Text Hurraaaaaaaa!!!!
+
+* [*] List
+* [ ] List
+
+
+.Das ist ein toller Titel
+|===
+|Name of Column 1 |Name of Column 2 |Name of Column 3
+
+|Cell in column 1, row 1
+|Cell in column 2, row 1
+|Cell in column 3, row 1
+
+|Cell in column 1, row 2
+|Cell in column 2, row 2
+|Cell in column 3, row 2
+|===
+
+* Link: https://asciidoctor.org - automatic!
+* Link: link:++https://example.org/?q=[a b]++[URL with special characters]
+
+* Bold: *Bold*
+* Italic: _Italic_
+* Monospace: `Monospace`
+
+* #Hash Hash#
+* [.underline]#Underline#
+* [.line-through]#Line-Through#
+
+* Super^script^
+* Sub~script~'),
+       ('Pizza', 'Italienische Pizza', '=== Belag
+
+* Tomatensauße
+* Salami');
