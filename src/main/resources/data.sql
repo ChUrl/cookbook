85a136211589068b4d7f5e35d0d76cd7aee01fae
ChUrl <tobi@urpost.de>
2021-02-26 13:08:40 +0100

    init example recipe

---
+++ src/main/resources/data.sql
@@ -0,0 +1,22 @@
+INSERT INTO recipes (recipe_id, recipe_title, recipe_descr, recipe_body)
+VALUES (10000, 'Kartoffelsalat', 'Ein Salat aus Kartoffeln, der Kartoffeln beinhaltet.',
+'### Benötigte Zutaten
+<hr>
+
+1. Kartoffeln
+2. Mehr Kartoffeln
+3. Salat
+4. Man braucht auch noch 1EL Kartoffeln
+
+### Zubereitung
+<hr>
+
+- Kartoffeln schälen
+- Die Schale essen
+
+++PlusPlus++<br>
+~~TildeTilde~~<br>
+*Stern*<br>
+**SternStern**<br>
+***SternSternStern***<br>
+<mark>MarkTag<mark/>');
