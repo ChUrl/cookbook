b0766fc6cfa33e60dead7098e969442f8cf37a0c
ChUrl <tobi@urpost.de>
2021-02-27 17:58:24 +0100

    clean controller from junk, use persistenceservice

--- src/main/java/de/churl/cookbook/infrastructure/controller/CookBookController.java
+++ src/main/java/de/churl/cookbook/infrastructure/controller/CookBookController.java
@@ -1,13 +1,9 @@
 package de.churl.cookbook.infrastructure.controller;
 
-import de.churl.cookbook.model.Ingredient;
 import de.churl.cookbook.model.IngredientType;
-import de.churl.cookbook.model.IngredientUsage;
-import de.churl.cookbook.model.IngredientUsageKey;
-import de.churl.cookbook.model.Recipe;
+import de.churl.cookbook.model.transfer.RecipeDTO;
 import de.churl.cookbook.service.PersistenceService;
-import de.churl.cookbook.persistance.IngredientRepository;
-import de.churl.cookbook.persistance.RecipeRepository;
+import de.churl.cookbook.utility.IdUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.commonmark.Extension;
@@ -25,9 +21,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 
@@ -38,78 +32,50 @@ public class CookBookController {
 
     private final PersistenceService persistenceService;
 
-    // TODO: Move to service
-    private final RecipeRepository recipeRepository;
-    private final IngredientRepository ingredientRepository;
-
     @GetMapping("*")
     public String index(Model model) {
-        model.addAttribute("recipes", this.recipeRepository.findAll());
+        model.addAttribute("recipeDTOs", this.persistenceService.findAllRecipes());
 
         return "all_recipe_list";
     }
 
     @GetMapping("/new")
     public String newRecipe(Model model) {
-        model.addAttribute("ingredients", this.ingredientRepository.findAll());
+        model.addAttribute("ingrDTOs", this.persistenceService.findAllIngredients());
 
         return "new_recipe_form";
     }
 
-    // TODO: Im Form müssen die Zutaten ausgewählt werden
     @PostMapping("/new/submit")
-    public String newRecipeSubmit(@RequestParam("recipe_title") String title,
-                                  @RequestParam("recipe_desc") String description,
-                                  @RequestParam("recipe_ingrs") Set<String> ingredients,
-                                  @RequestParam("recipe_body") String body) {
+    public String newRecipeSubmit(@RequestParam("recipe_title") String recipeTitle,
+                                  @RequestParam("recipe_descr") String recipeDescr,
+                                  @RequestParam("recipe_body") String recipeBody,
+                                  @RequestParam("recipe_ingrs") Set<String> recipeIngrs) {
 
-        // TODO: Use recipe DTO with requestbody?
-        // TODO: Move to service
-        final Recipe r = new Recipe();
-        r.setRecipeID(UUID.randomUUID());
-        r.setRecipeTitle(title);
-        r.setRecipeDescr(description);
-        r.setRecipeBody(body);
-        r.setIngredientUsages(new HashSet<>());
-        for (String ingr : ingredients) {
-            Optional<Ingredient> ingropt = this.ingredientRepository.findById(UUID.fromString(ingr));
-            if (ingropt.isPresent()) {
-                Ingredient ingredient = ingropt.get();
-                IngredientUsageKey key = new IngredientUsageKey();
-                key.setRecipeID(r.getRecipeID());
-                key.setIngrID(UUID.fromString(ingr));
-                IngredientUsage usage = new IngredientUsage();
-                usage.setIngredientUsageKey(key);
-                usage.setRecipe(r);
-                usage.setIngredient(ingredient);
-                usage.setAmount(1);
-                r.getIngredientUsages().add(usage);
-            }
-        }
-        this.recipeRepository.save(r);
+        final UUID recipeID = this.persistenceService.saveNewRecipe(recipeTitle,
+                                                                    recipeDescr,
+                                                                    recipeBody,
+                                                                    IdUtil.toUUID(recipeIngrs));
 
-        return "redirect:/details/" + r.getRecipeID();
+        return "redirect:/details/" + recipeID;
     }
 
     @GetMapping("/details/{id}")
-    public String details(@PathVariable("id") String id,
+    public String details(@PathVariable("id") String recipeID,
                           Model model) {
 
-        final Optional<Recipe> ropt = this.recipeRepository.findById(UUID.fromString(id));
-        if (ropt.isPresent()) {
+        final RecipeDTO recipeDTO = this.persistenceService.findRecipeById(recipeID);
 
-            final List<Extension> extensions = Arrays.asList(AutolinkExtension.create(),
-                                                             StrikethroughExtension.create(),
-                                                             InsExtension.create());
+        // Commonmark Markdown Parser
+        final List<Extension> extensions = Arrays.asList(AutolinkExtension.create(),
+                                                         StrikethroughExtension.create(),
+                                                         InsExtension.create());
+        final Parser parser = Parser.builder().extensions(extensions).build();
+        final Node node = parser.parse(recipeDTO.getBody());
+        final HtmlRenderer render = HtmlRenderer.builder().extensions(extensions).build();
 
-            final Recipe recipe = ropt.get();
-            final Parser parser = Parser.builder().extensions(extensions).build();
-            final Node node = parser.parse(recipe.getRecipeBody());
-            final HtmlRenderer render = HtmlRenderer.builder().extensions(extensions).build();
-
-            model.addAttribute("recipe", recipe);
-            model.addAttribute("bodyhtml", render.render(node));
-        }
+        model.addAttribute("recipeDTO", recipeDTO);
+        model.addAttribute("bodyhtml", render.render(node));
 
         return "recipe_detail_view";
     }
@@ -119,9 +85,9 @@ public class CookBookController {
     // @PostMapping("/details/{id}/update")
 
     // TODO: Zutaten hinzufügen, entfernen
-    // @GetMapping("/details/{id}/ingredients")
-    // @PostMapping("/details/{id}/ingredients/add/{ingredient}")
-    // @PostMapping("/details/{id}/ingredients/remove/{ingredient}")
+    // @GetMapping("/details/{id}/ingrDTOs")
+    // @PostMapping("/details/{id}/ingrDTOs/add/{ingredient}")
+    // @PostMapping("/details/{id}/ingrDTOs/remove/{ingredient}")
 
     @GetMapping("/timeline")
     public String timeline() {
@@ -131,7 +97,7 @@ public class CookBookController {
     @GetMapping("/ingredients")
     public String ingredients(Model model) {
         // TODO: Use service
-        model.addAttribute("ingredients", this.ingredientRepository.findAll());
+        model.addAttribute("ingrDTOs", this.persistenceService.findAllIngredients());
 
         return "all_ingr_list";
     }
@@ -139,7 +105,7 @@ public class CookBookController {
     @GetMapping("/ingredients/new")
     public String newIngredient(Model model) {
         // TODO: Move from enum to db table?
-        model.addAttribute("types", Arrays.asList(IngredientType.values()));
+        model.addAttribute("ingrTypes", Arrays.asList(IngredientType.values()));
 
         return "new_ingr_form";
     }
@@ -147,18 +113,14 @@ public class CookBookController {
     @PostMapping("/ingredients/new/submit")
     public String newIngredientSubmit(@RequestParam("ingredient_title") String title,
                                       @RequestParam("ingredient_type") String type) {
-        // TODO: Move to service
-        final Ingredient ingredient = new Ingredient();
-        ingredient.setIngrID(UUID.randomUUID());
-        ingredient.setIngrTitle(title);
-        ingredient.setIngrType(IngredientType.valueOf(type));
-        this.ingredientRepository.save(ingredient);
 
-        return "redirect:/ingredients";
+        final UUID ingrID = this.persistenceService.saveNewIngredient(title, type);
+
+        return "redirect:/ingredients/details/" + ingrID;
     }
 
-    // TODO: @PostMapping("/ingredients/remove")
-    // TODO: @GetMapping("/ingredients/details/{id}")
+    // TODO: @PostMapping("/ingrDTOs/remove")
+    // TODO: @GetMapping("/ingrDTOs/details/{id}")
 
     @GetMapping("/search")
     public String search() {
