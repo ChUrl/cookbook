e56d9feee912b00f71ad4ba5c79542ec20b7d7d2
churl <tobi@urpost.de>
2021-04-01 21:18:54 +0200

    update dto

--- src/main/kotlin/de/churl/cookbook/model/transfer/RecipeDTO.kt
+++ src/main/kotlin/de/churl/cookbook/model/transfer/RecipeDTO.kt
@@ -5,4 +5,24 @@ data class RecipeDTO(
     val descr: String = "",
     val body: String = "",
     val ingrs: Collection<String> = emptySet()
-)
+) {
+    fun withTitle(title: String): RecipeDTO {
+        return RecipeDTO(title, descr, body, ingrs)
+    }
+
+    fun withDescr(descr: String): RecipeDTO {
+        return RecipeDTO(title, descr, body, ingrs)
+    }
+
+    fun withBody(body: String): RecipeDTO {
+        return RecipeDTO(title, descr, body, ingrs)
+    }
+
+    fun withIngr(ingr: String): RecipeDTO {
+        return RecipeDTO(title, descr, body, (ingrs + ingr).distinct().toSet())
+    }
+
+    fun withoutIngr(ingr: String): RecipeDTO {
+        return RecipeDTO(title, descr, body, (ingrs - ingr).distinct().toSet())
+    }
+}
